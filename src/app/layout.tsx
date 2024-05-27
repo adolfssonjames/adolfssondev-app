93008d42873ccc661294b54341e8b0b6937827c9
James <adolfssonjames@gmail.com>
2024-05-27 12:53:35 +0200

    header /w  navbar complete design with framer-motion.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
+import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Adolfsson Dev",
+  title: "Adolfsson Digital",
   description:
-    "Vi utvecklar webbsidor, intranät och erbjuder konsulttjänster. Professionellt och Prisvärt",
+    "Vi utvecklar webbsidor och erbjuder konsulttjänster. Professionellt och Prisvärt",
 };
 
 export default function RootLayout({
@@ -17,12 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-slate-700 text-slate-50 relative`}
+        className={`${inter.className} bg-slate-700 text-slate-50 relative h-[5000px]`}
       >
         <div className="bg-slate-500 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-indigo-300 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-
-        {children}
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
