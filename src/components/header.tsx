83f1c6aee5635296dbde3566f3f3e4ea8ac2e96e
James <adolfssonjames@gmail.com>
2024-06-04 01:56:00 +0200

    gap on nav fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,8 +21,9 @@ export default function Header() {
       <div className="text-lg fixed top-[0.15rem]  left-0 h-12 py-2 sm:top-[1.7rem] pl-2 sm:pl-10 sm:left-0 sm:h-[initial] sm:py-0">
         <Link href={"/"}>Adolfsson Digital</Link>
       </div>
-      <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+
+      <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 ">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-3">
           {links.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
