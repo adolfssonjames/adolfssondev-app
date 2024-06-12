6ebf36aa8b3789dc30522839320efaa39190737e
James <adolfssonjames@gmail.com>
2024-06-12 02:53:39 +0200

    theme background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
       <body
         className={`${font.className} bg-slate-800 text-slate-50 relative pt-28 sm:pt-36 dark:bg-slate-100`}
       >
-        <div className="bg-sky-950 absolute top-[-12rem] -z-10 right-[1rem] h-[70.25rem] w-[12rem] rounded-full blur-[5rem] sm:w-[100.75rem] md:right-[-43rem] lg:right-[-28rem] dark:bg-cyan-50"></div>
-        <div className="bg-cyan-950 absolute top-[-14rem] -z-10 left-[-35rem] h-[72.25rem] w-[40rem] rounded-full blur-[5rem] sm:w-[80.75rem] md:left-[-23rem]  lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-green-50"></div>
+        <div className="bg-sky-950 absolute top-[-12rem] -z-10 right-[1rem] h-[75.25rem] w-[50rem] rounded-full blur-[5rem] sm:w-[100.75rem] md:right-[-43rem] lg:right-[-28rem] dark:bg-cyan-50"></div>
+        <div className="bg-cyan-950 absolute top-[-28rem] -z-10 left-[-35rem] h-[96.25rem] w-[50rem] rounded-full blur-[5rem] sm:w-[75.75rem] md:left-[-23rem]  lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-green-50"></div>
         <ActiveSectionContextProvider>
           <ThemeContextProvider>
             <Header />
