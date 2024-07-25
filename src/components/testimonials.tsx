c9e7d0c796c021df5b1c6c0b6ea3d25ae2ba4737
James <adolfssonjames@gmail.com>
2024-07-25 20:33:02 +0200

    added padding in testimonial text

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -38,7 +38,7 @@ export default function Testimonials() {
             >
               <p className="font-bold">{testimonial.name}</p>
               <p className="font-medium">{testimonial.role}</p>
-              <p className="italic">{testimonial.testimonial}</p>
+              <p className="italic pt-2">{testimonial.testimonial}</p>
             </motion.li>
           ))}
         </ul>
