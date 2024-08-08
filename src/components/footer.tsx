df79d26c2db8865aeee4067687505172d651757c
James <adolfssonjames@gmail.com>
2024-08-08 16:37:09 +0200

    added name to company info

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -40,7 +40,7 @@ export default function Footer() {
         </div>
         <div className="flex flex-col">
           <small> &copy; 2024 Adolfsson Digital </small>{" "}
-          <small> Kryssvägen 5, 13953, Stockholm </small>{" "}
+          <small> James Adolfsson, Kryssvägen 5, 13953, Stockholm </small>{" "}
           <small> Org.nr: 19950902XXXX - lämnas på begäran </small>{" "}
           <small> Godkänd för F-skatt </small>{" "}
         </div>
